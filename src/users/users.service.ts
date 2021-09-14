40319787bdb280c09a8d7ee429e3cb71039c89a3
Tarik Tarik <tarikpnr147@gmail.com>
2021-09-14 19:27:40 +0300

    basic auth api initialized

---
+++ src/users/users.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+
+  find(email: string) {
+    return this.repo.find({ email });
+  }
+
+  findOne(id: number) {
+    return this.repo.findOne(id);
+  }
+
+  async update(id: number, attributes: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found with the given id');
+    }
+    Object.assign(user, attributes);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found with the given id');
+    }
+    return this.repo.remove(user);
+  }
+}
